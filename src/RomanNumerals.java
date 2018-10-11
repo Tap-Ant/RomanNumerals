d97bc8b1c57100cd79b432d44bf1c7341946d89e
tapantti <tapantti@W10-156.student.yo.oulu.fi>
2018-10-11 15:17:25 +0300

    Commit 1 11.10. 15:17

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
 		return 0;
+		
+	}
+	public void checkInput(String romanNum) throws IllegalArgumentException{
+		Pattern p1 = Pattern.compile("([a-z\\d])\\1\\1\\1", Pattern.CASE_INSENSITIVE);
+	    Matcher m1 = p1.matcher(romanNum);
+	    if (m1.find()) throw new IllegalArgumentException();
+	    							//V, L, or D 2 times
+	    Pattern p2 = Pattern.compile("([a-z\\d])\\1", Pattern.CASE_INSENSITIVE);
+	    Matcher m2 = p2.matcher(romanNum);
+	    if (m2.find()) throw new IllegalArgumentException();
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckInputIXCM() throws IllegalArgumentException{
+		RomanNumerals rm = new RomanNumerals();
+		String roman1 = "IIII";
+		String roman2 = "IIIXXXMMMM";
+		String roman3 = "MCMXXXX";
+		
+		rm.checkInput(roman1);
+		rm.checkInput(roman2);
+		rm.checkInput(roman3);
+		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCheckInputVLD() throws IllegalArgumentException{
+		RomanNumerals rm = new RomanNumerals();
+		String roman1 = "VV";
+		String roman2 = "MLL";
+		String roman3 = "DDD";
+		
+		rm.checkInput(roman1);
+		rm.checkInput(roman2);
+		rm.checkInput(roman3);
+		
 	}
 
 }
